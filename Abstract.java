e12489ca74664dbb9c9dd789da63864e57207b7e
Komal <komalanmol2509@gmail.com>
2026-02-08 19:30:44 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,23 @@
+package unit2;
+
+public class Abstract extends Box implements PureAbstractClass{
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double circumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double voulume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
